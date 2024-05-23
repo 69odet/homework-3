53acbe6c6c9bfcb127b338126110036a790563d3
69odet <odet.o@yandex.ru>
2024-05-23 22:31:38 +0300

    Add files via upload

---
+++ homewprk3.py
@@ -0,0 +1,8 @@
+my_string = input('Я живу в красивом городе, ')
+print(len(my_string))
+print('Я живу в красивом городе'.upper())
+print('Я живу в красивом городе'.lower())
+print('Я живу в красивом городе'.replace(' ', ''))
+print('Я живу в красивом городе'.replace(' ', '#'))
+print(my_string[0])
+print(my_string[-1])
